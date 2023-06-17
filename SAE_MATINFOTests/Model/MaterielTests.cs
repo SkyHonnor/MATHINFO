c3a22de8677e9b4b9a4ac94190e7681d26648ad8
SkyHonnor <teamruault.maxime2@gmail.com>
2023-06-17 18:47:43 +0200

    Matériel test maj

--- SAE_MATINFOTests/Model/MaterielTests.cs
+++ SAE_MATINFOTests/Model/MaterielTests.cs
@@ -12,15 +12,22 @@ namespace SAE_MATINFO.Model.Tests
     [TestClass()]
     public class MaterielTests
     {
+        private static Categorie categorie;
 
+        [TestInitialize()]
+        public void Initialize()
+        {
+            categorie = new Categorie("Ordinateur");
+            categorie.Create();
+        }
 
         [TestMethod()]
-        public void MaterielTest()
+        public void CreateTest()
         {
-            Materiel materiel1 = new Materiel();
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
             materiel1.Create();
 
-            Materiel materiel2 = new Materiel();
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
             materiel2.Read();
 
             Assert.AreEqual(materiel1, materiel2);
@@ -29,56 +36,81 @@ namespace SAE_MATINFO.Model.Tests
         }
 
         [TestMethod()]
-        public void MaterielTest1()
-        {
-            ClearAll();
-        }
-
-        [TestMethod()]
-        public void MaterielTest2()
-        {
-            ClearAll();
-        }
-
-        [TestMethod()]
-        public void MaterielTest3()
-        {
-            ClearAll();
-        }
-
-        [TestMethod()]
-        public void CreateTest()
-        {
-            ClearAll();
-        }
-
-        [TestMethod()]
         public void DeleteTest()
         {
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel1.Create();
+
+            materiel1.Delete();
+
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel2.Read();
+
+            Assert.AreEqual(0, materiel2.IdMateriel);
+
             ClearAll();
         }
 
         [TestMethod()]
         public void ReadTest()
         {
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel1.Create();
+
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel2.Read();
+
+            Assert.AreEqual(materiel1, materiel2);
+
             ClearAll();
         }
 
         [TestMethod()]
         public void UpdateTest()
         {
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel1.Create();
+
+            materiel1.NomMateriel = "Imprimante";
+            materiel1.Update();
+
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "Imprimante", "1010101010", "REF604");
+            materiel2.Read();
+
+            Assert.AreEqual(materiel1, materiel2);
+
             ClearAll();
         }
 
         [TestMethod()]
         public void FindAllTest()
         {
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel1.Create();
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "Imprimante", "1010101010", "REF604");
+            materiel2.Create();
+            Materiel materiel3 = new Materiel(categorie.IdCategorie, "PC de ouf", "1010101010", "REF604");
+            materiel3.Create();
+
+            ObservableCollection<Materiel> lesMateriels = new Materiel().FindAll();
+            Assert.AreEqual(3, lesMateriels.Count);
+
             ClearAll();
         }
 
         [TestMethod()]
         public void FindBySelectionTest()
         {
+            Materiel materiel1 = new Materiel(categorie.IdCategorie, "PC Dell (pas ouf)", "1010101010", "REF604");
+            materiel1.Create();
+            Materiel materiel2 = new Materiel(categorie.IdCategorie, "Imprimante", "1010101010", "REF604");
+            materiel2.Create();
+            Materiel materiel3 = new Materiel(categorie.IdCategorie, "PC de ouf", "1010101010", "REF604");
+            materiel3.Create();
+
+            ObservableCollection<Materiel> lesMateriels = new Materiel().FindBySelection("nom_materiel = 'PC de ouf'");
+            Assert.AreEqual(1, lesMateriels.Count);
+
             ClearAll();
         }
 
@@ -90,11 +122,8 @@ namespace SAE_MATINFO.Model.Tests
                 mat.Delete();
             }
 
-            ObservableCollection<Categorie> lesCategorie = new Categorie().FindAll();
-            foreach (Categorie categorie in lesCategorie)
-            {
-                categorie.Delete();
-            }
         }
+
+        
     }
 }
