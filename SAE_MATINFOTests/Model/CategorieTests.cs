86683af5c02c4e34e7829a62b0ede9054423d9e1
SkyHonnor <teamruault.maxime2@gmail.com>
2023-06-17 17:49:56 +0200

    Mise à jour des testes

---
+++ SAE_MATINFOTests/Model/CategorieTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_MATINFO.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_MATINFO.Model.Tests
+{
+    [TestClass()]
+    public class CategorieTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+
+            Categorie categorie2 = new Categorie("Ordinateur");
+            categorie2.Read();
+
+            Assert.AreEqual(categorie1, categorie2);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+
+            categorie1.Delete();
+
+            Categorie categorie2 = new Categorie("Ordinateur");
+            categorie2.Read();
+
+            Assert.AreEqual(0, categorie2.IdCategorie);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+
+            Categorie categorie2 = new Categorie("Ordinateur");
+            categorie2.Read();
+
+            Assert.AreEqual(categorie1, categorie2);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+
+            categorie1.NomCategorie = "Imprimante";
+            categorie1.Update();
+
+            Categorie personnel2 = new Categorie("Imprimante");
+            personnel2.Read();
+
+            Assert.AreEqual(personnel2, categorie1);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void FindAllTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+            Categorie categorie2 = new Categorie("Imprimante");
+            categorie2.Create();
+            Categorie categorie3 = new Categorie("Informatique");
+            categorie3.Create();
+
+            ObservableCollection<Personnel> lesCategorie = new Personnel().FindAll();
+            Assert.AreEqual(3, lesCategorie.Count);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void FindBySelectionTest()
+        {
+            Categorie categorie1 = new Categorie("Ordinateur");
+            categorie1.Create();
+            Categorie categorie2 = new Categorie("Imprimante");
+            categorie2.Create();
+            Categorie categorie3 = new Categorie("Informatique");
+            categorie3.Create();
+
+            ObservableCollection<Categorie> lesCategorie = new Categorie().FindBySelection("nom_categorie = 'Imprimante'");
+            Assert.AreEqual(1, lesCategorie.Count);
+
+            ClearAll();
+        }
+
+        private void ClearAll()
+        {
+            ObservableCollection<Categorie> lesCategorie = new Categorie().FindAll();
+            foreach (Categorie categorie in lesCategorie)
+            {
+                categorie.Delete();
+            }
+        }
+    }
+}
---
+++ SAE_MATINFOTests/Model/MaterielTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAE_MATINFO.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_MATINFO.Model.Tests
+{
+    [TestClass()]
+    public class MaterielTests
+    {
+
+
+        [TestMethod()]
+        public void MaterielTest()
+        {
+            Materiel materiel1 = new Materiel();
+            materiel1.Create();
+
+            Materiel materiel2 = new Materiel();
+            materiel2.Read();
+
+            Assert.AreEqual(materiel1, materiel2);
+
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void MaterielTest1()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void MaterielTest2()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void MaterielTest3()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void ReadTest()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void FindAllTest()
+        {
+            ClearAll();
+        }
+
+        [TestMethod()]
+        public void FindBySelectionTest()
+        {
+            ClearAll();
+        }
+
+        private void ClearAll()
+        {
+            ObservableCollection<Materiel> lesMateriaux = new Materiel().FindAll();
+            foreach (Materiel mat in lesMateriaux)
+            {
+                mat.Delete();
+            }
+
+            ObservableCollection<Categorie> lesCategorie = new Categorie().FindAll();
+            foreach (Categorie categorie in lesCategorie)
+            {
+                categorie.Delete();
+            }
+        }
+    }
+}
