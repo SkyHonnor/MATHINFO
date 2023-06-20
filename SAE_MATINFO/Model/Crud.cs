c19720cbdc549e75c6f52226803fa824ab34eeab
SkyHonnor <maxime.ruault@etu.univ-smb.fr>
2023-06-20 11:09:48 +0200

    Modification Documentation

--- SAE_MATINFO/Model/Crud.cs
+++ SAE_MATINFO/Model/Crud.cs
@@ -2,6 +2,10 @@ using System.Collections.ObjectModel;
 
 namespace SAE_MATINFO.Model
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface Crud<T>
    {
       void Create();
